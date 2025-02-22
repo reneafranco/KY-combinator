6092660e74482bffc209683a8e359f606a8f6721
reneafranco <reneadonayfranco@gmail.com>
2025-02-21 19:06:25 -0500

    start point

---
+++ demo/src/main/java/com/x10dev/demo/KyCombinatorApplication.java
@@ -0,0 +1,13 @@
+package com.x10dev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyCombinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyCombinatorApplication.class, args);
+	}
+
+}

9b516677d63eb885bbc796f8dc18e3aa44c5dad5
reneafranco <reneadonayfranco@gmail.com>
2025-02-21 19:06:25 -0500

    start point

---
+++ demo/src/test/java/com/x10dev/demo/KyCombinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x10dev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyCombinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

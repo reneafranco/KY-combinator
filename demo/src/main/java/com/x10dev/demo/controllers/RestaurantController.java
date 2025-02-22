d63f746b0a8dffa0fb1326e39bb9d8ae39c2e4c8
reneafranco <reneadonayfranco@gmail.com>
2025-02-21 23:32:49 -0500

    first api

---
+++ demo/src/main/java/com/x10dev/demo/controllers/RestaurantController.java
@@ -0,0 +1,39 @@
+package com.x10dev.demo.controllers;
+
+import com.x10dev.demo.entity.Restaurant;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class RestaurantController {
+
+    private final ChatClient chatClient;
+
+
+    public RestaurantController(ChatClient.Builder chatClient) {
+        this.chatClient = chatClient.build();
+    }
+
+    @GetMapping("/dad-jokes")
+    public List<Restaurant> generate() {
+        String msg = "recommend me resturants in louisville KY";
+
+//        PromptTemplate promptTemplate = new PromptTemplate(msg);
+//        Prompt prompt = promptTemplate.create(Map.of("message", msg));
+
+        return this.chatClient.prompt().user(msg).call().entity(new ParameterizedTypeReference<List<Restaurant>>() {});
+
+    }
+
+
+
+
+}
